bb49c7fd9950f5e4249ac86b56c813fd374ced88
Naga Venkatesh Gavini <nagavenkateshgavini@nagas-air.myfiosgateway.com>
2022-09-23 22:42:33 -0400

    first commit

---
+++ lab3_student/src/edu/northeastern/csye6200/LAB3_P1.java
@@ -0,0 +1,55 @@
+package edu.northeastern.csye6200;
+
+public class LAB3_P1 {
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+
+	/** Return true if the card number is valid */
+	public static boolean isValid(long number) {
+		// TODO: write your code here
+		return true;
+	}
+
+	/** Get the result from Step 2 */
+	public static int sumOfDoubleEvenPlace(long number) {
+		// TODO: write your code here
+		return 0;
+	}
+
+	/**
+	 * Return this number if it is a single digit, otherwise, return the sum of
+	 * the two digits
+	 */
+	public static int getDigit(int number) {
+		// TODO: write your code here
+		return 0;
+	}
+
+	/** Return sum of odd place digits in number */
+	public static int sumOfOddPlace(long number) {
+		// TODO: write your code here
+		return 0;
+	}
+
+	/** Return true if the digit d is a prefix for number */
+	public static boolean prefixMatched(long number, int d) {
+		// TODO: write your code here
+		return true;
+	}
+
+	/** Return the number of digits in d */
+	public static int getSize(long d) {
+		// TODO: write your code here
+		return 0;
+	}
+
+	/**
+	 * Return the first k number of digits from number. If the number of digits
+	 * in number is less than k, return number.
+	 */
+	public static long getPrefix(long number, int k) {
+		// TODO: write your code here
+		return 0;
+	}
+}
---
+++ lab3_student/src/edu/northeastern/csye6200/LAB3_P2.java
@@ -0,0 +1,14 @@
+package edu.northeastern.csye6200;
+
+public class LAB3_P2 {
+	public static void main(String[] args) {
+		// TODO: write your code here
+	}
+
+	public static boolean isConsecutiveFour(int[] values) {
+		// TODO: write your code here
+		return true;
+	}
+
+}
+
