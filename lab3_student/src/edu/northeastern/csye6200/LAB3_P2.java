967d7de5b3ee3eba638390524efa09fccd8511a6
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2022-09-25 14:22:55 -0400

    second program commit

--- lab3_student/src/edu/northeastern/csye6200/LAB3_P2.java
+++ lab3_student/src/edu/northeastern/csye6200/LAB3_P2.java
@@ -1,13 +1,53 @@
 package edu.northeastern.csye6200;
 
+import java.util.Scanner;
+
 public class LAB3_P2 {
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter the number of values: ");
+		int totalValues = scanner.nextInt();
+		
+		int[] numbers = new int[totalValues];
+		try {
+			System.out.print("Enter the numbers as an array:");		
+			numbers = new int[totalValues];
+			for(int i = 0;i < totalValues;i++){
+			   numbers[i] = scanner.nextInt();
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}		
+		
+		boolean isConsecutiveFourExists = isConsecutiveFour(numbers);
+		if(isConsecutiveFourExists) {
+			System.out.println("The list has consecutive fours");
+		}else {
+			System.out.println("The list has no consecutive fours");
+		}
 	}
 
 	public static boolean isConsecutiveFour(int[] values) {
-		// TODO: write your code here
-		return true;
+		int counter = 0;
+		int previous = values[0];
+		for(int i=1; i < values.length; i++) {
+			if(values[i] == previous) {
+				counter++;
+			}
+			previous = values[i];
+			
+			if(i==(values.length-1)) {
+				if(values[i]==previous) {
+					counter++;
+				}
+			}
+
+			if(counter==4) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
